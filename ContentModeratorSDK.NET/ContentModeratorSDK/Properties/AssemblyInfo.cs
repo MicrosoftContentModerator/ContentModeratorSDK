2b6ddcf47b45e4656e5493aba27d557201dbc6fc
Federico Gomez Suarez <federigo@microsoft.com>
2015-10-15 14:36:42 -0700

    Adding nuspec for project

--- ContentModeratorSDK.NET/ContentModeratorSDK/Properties/AssemblyInfo.cs
+++ ContentModeratorSDK.NET/ContentModeratorSDK/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ContentModeratorSDK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Content Moderator SDK.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ContentModeratorSDK")]

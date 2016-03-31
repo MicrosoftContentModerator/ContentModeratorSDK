e5a48cdb75505a20bbe6b6f6a147ffb7b51ec118
swiftarrow11 <suraks@microsoft.com>
2016-03-30 18:17:42 -0700

    Updated PDNA testd

--- ContentModeratorSDK.NET/ContentModeratorSDK.Tests/PDNAServiceTests.cs
+++ ContentModeratorSDK.NET/ContentModeratorSDK.Tests/PDNAServiceTests.cs
@@ -6,11 +6,16 @@ using ContentModeratorSDK.Image;
 
 namespace ContentModeratorSDK.Tests
 {
+    using System.IO;
+    using System.Linq;
+    using Newtonsoft.Json;
+
     [TestClass]
     public class PDNAServiceTests
     {
         private const string TestImageUrl = "https://cdn.schedulicity.com/usercontent/b9de6e06-e954-4169-ac44-57fa1c3b4245.jpg";
-        public const string TestImageContent = @"Content\sample.jpg";
+        public const string noMatchImageContent = @"Content\sample.jpg";
+        public const string matchImageContent = @"C:\Users\suraks\Downloads\SampleImages224\SampleImages224\img_130.jpg";//Content\pdnasample.jpg";
 
         public const string TestTags = "101,102";
         public const string TestLabel = "TestImage";
@@ -39,7 +44,7 @@ namespace ContentModeratorSDK.Tests
         /// </summary>
         [TestMethod]
         [TestCategory("PDNASvc")]
-        public void ValidateImageSimple()
+        public void ValidateImageUrl()
         {
             IPDNAService pdnaService = new PDNAService(this.pdnaServiceOptions);
             Service.Results.MatchImageResult extractResult = ValidateImageContent(pdnaService, false);
@@ -49,32 +54,81 @@ namespace ContentModeratorSDK.Tests
             Assert.IsNotNull(extractResult.MatchDetails, "Expected valid result");
         }
 
+        /// <summary>
+        /// Simply calling the PDNA Cloud Service with the Test Image without Caching
+        /// </summary>
+        [TestMethod]
+        [TestCategory("PDNASvc")]
+        public void ValidateImageBinaryContentMatch()
+        {
+            using (Stream stream = new FileStream(matchImageContent, FileMode.Open, FileAccess.Read))
+            {
+                IPDNAService pdnaService = new PDNAService(this.pdnaServiceOptions);
+
+                ImageModeratableContent imageContent = new ImageModeratableContent(new BinaryContent(stream, "image/jpeg"));
+                var moderateResult = pdnaService.ValidateImageAsync(imageContent,false);
+                var actualResult = moderateResult.Result;
+                Assert.IsTrue(actualResult != null, "Expected valid result, Response: {0}", JsonConvert.SerializeObject(actualResult));
+                Assert.IsTrue(actualResult.MatchDetails.MatchFlags.Count() >0, "Expected Match Count to be greater than 0, Response: {0}", JsonConvert.SerializeObject(actualResult));
+
+            }
+        }
+
+        /// <summary>
+        /// Simply calling the PDNA Cloud Service with the Test Image without Caching
+        /// </summary>
+        [TestMethod]
+        [TestCategory("PDNASvc")]
+        public void ValidateImageBinaryContentNoMatch()
+        {
+            using (Stream stream = new FileStream(noMatchImageContent, FileMode.Open, FileAccess.Read))
+            {
+                IPDNAService pdnaService = new PDNAService(this.pdnaServiceOptions);
+
+                ImageModeratableContent imageContent = new ImageModeratableContent(new BinaryContent(stream, "image/jpeg"));
+                var moderateResult = pdnaService.ValidateImageAsync(imageContent, false);
+                var actualResult = moderateResult.Result;
+                Assert.IsTrue(actualResult != null, "Expected valid result, Response: {0}", JsonConvert.SerializeObject(actualResult));
+                Assert.IsTrue(actualResult.MatchDetails.MatchFlags.Count() == 0, "No Match was expected for this image, Response: {0}", JsonConvert.SerializeObject(actualResult));
+
+            }
+        }
+
+
 
         /// <summary>
         /// First call the Validate method with Image Caching set to true and get back the CacheId
         /// Next call the Validate method again sending in the Cache Id that was generated in the previous call
         /// </summary>
-        //[TestMethod]
-        //[TestCategory("PDNASvc")]
-        //public void ValidateImageInCache()
-        //{
-        //    IPDNAService pdnaService = new PDNAService(this.pdnaServiceOptions);
-        //    var actualResult = ValidateImageContent(pdnaService, true);
+        [TestMethod]
+        [TestCategory("PDNASvc")]
+        public void ValidateImageInCache()
+        {
+            IPDNAService pdnaService = new PDNAService(this.pdnaServiceOptions);
+            var actualResult = ValidateImageContent(pdnaService, true);
 
-        //    var pdnaResult = pdnaService.ValidateImageInCache(actualResult.CacheID);
-        //    actualResult = pdnaResult.Result;
-        //    Assert.IsTrue(actualResult != null, "Expected valid result");
-        //    Assert.IsTrue(actualResult.IsMatch, "Expected valid result");
-        //    Assert.IsNotNull(actualResult.MatchDetails, "Expected valid result");
-        //}
+            Assert.IsTrue(actualResult != null, "Expected valid result");
+            Assert.IsTrue(actualResult.IsMatch, "Expected valid result");
+            Assert.IsNotNull(actualResult.CacheID, "Cache ID should not be null");
+
+
+            var pdnaResult = pdnaService.ValidateImageInCache(actualResult.CacheID);
+            var matchRes = pdnaResult.Result;
+            Assert.IsTrue(matchRes != null, "Expected valid result");
+            Assert.IsTrue(matchRes.IsMatch, "Expected valid result");
+            Assert.IsNotNull(matchRes.MatchDetails, "Expected valid result");
+        }
 
         private static Service.Results.MatchImageResult ValidateImageContent(IPDNAService pdnaService, bool cacheImage = false)
         {
             ImageModeratableContent imageContent =
-                new ImageModeratableContent("https://wbintcvsstorage.blob.core.windows.net/matchedimages/img_130.jpg");
+                new ImageModeratableContent("https://pdnasampleimages.blob.core.windows.net/matchedimages/img_130.jpg");
             var extractResponse = pdnaService.ValidateImageAsync(imageContent, cacheImage);
             return extractResponse.Result;
 
         }
+
+
+        
     }
 }
